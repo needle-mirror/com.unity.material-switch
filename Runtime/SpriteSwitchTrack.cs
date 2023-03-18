ed1c430476a2f90d6235252c9e7c2aa19034eba5
Unity Technologies <@unity>
2023-03-18 00:00:00 +0000

    com.unity.material-switch@0.9.0-preview ## [0.9.0-preview] - 2023-03-18
    ### Added
    * feat: SpriteSwitch
    
    ### Changed
    * deps: use com.unity.film-internal-utilities@0.18.3-preview
    * doc: update the required Unity versions

---
+++ Runtime/SpriteSwitchTrack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Unity.FilmInternalUtilities;
+using Unity.SelectionGroups;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Unity.MaterialSwitch
+{
+    /// <summary>
+    /// Tracks for changing between sprites parameters. 
+    /// </summary>
+    [TrackClipType(typeof(SpriteSwitchClip))]
+    [TrackBindingType(typeof(SelectionGroup))]
+    internal class SpriteSwitchTrack : TrackAsset
+    {
+        
+        public override Playable CreateTrackMixer(UnityEngine.Playables.PlayableGraph graph, UnityEngine.GameObject go, int inputCount)
+        {
+#if UNITY_EDITOR
+            List<TimelineClip> clips        = new List<TimelineClip>(GetClips());
+            AnalyticsSender.SendEventInEditor(new SpriteSwitchTrackMixerEvent(clips.Count));            
+#endif                        
+            return ScriptPlayable<SpriteSwitchMixerPlayableBehaviour>.Create(graph, inputCount);
+        }
+    }
+}
